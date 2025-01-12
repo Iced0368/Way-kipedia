9340a11929c3f4ddf5afb212485792c3ba30966a
Iced0368 <iced03682@gmail.com>
2025-01-12 17:13:36 +0900

    refactor: event delegation from route-block to route-container

--- src/components/PageRoutes.tsx
+++ src/components/PageRoutes.tsx
@@ -7,17 +7,25 @@ const PageRoutes = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="route-container">
+        <div className="route-container"
+            onClick={(e) => {
+                const target = e.target;
+                if (target instanceof HTMLSpanElement 
+                    && target.className === "route-block"
+                    && target.textContent !== null
+                ) {
+                    const title = target.textContent;
+                    revertTo(title);
+                    navigate(`/nav/title=${title}`, {state: {title: title}})
+                }
+            }}
+        >
             지나온 문서들 :&nbsp;&nbsp;
             {
                 titleStack.map((title, i) => (
                     <span 
                         key={`${title}/${i}`}
                         className="route-block"
-                        onClick={() => {
-                            revertTo(title);
-                            navigate(`/nav/title=${title}`, {state: {title: title}})
-                        }}
                     >
                         {title}
                     </span>
